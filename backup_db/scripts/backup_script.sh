92130a77e9477206134db3dad338981419a94750
Borja Esteban <boressan@outlook.com>
2023-10-24 15:53:04 +0200

    mv install_cron into dockerfile and fix compose redundancy

--- backup_db/scripts/backup_script.sh
+++ backup_db/scripts/backup_script.sh
@@ -3,7 +3,7 @@
 # Database details
 DB_NAME="mlflow_db"
 DB_USER="mlflow_u"
-BACKUP_DIR="/backups"
+BACKUP_DIR="/backup_files"
 
 # Generate a timestamp for the backup file name
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
--- backup_db/scripts/backup_script_sqlite.sh
+++ backup_db/scripts/backup_script_sqlite.sh
@@ -3,11 +3,11 @@
 # Database details
 DB_NAME="mlflow_db"
 DB_USER="mlflow_u"
-BACKUP_DIR="/backups"
+BACKUP_DIR="/backup_files"
 
 # Generate a timestamp for the backup file name
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 
 #create the incremental backup copy
-pg_basebackup -U "$DB_USER" -D /backups/"$BACKUP_DIR/backup_inc-$TIMESTAMP" -Ft -x
+pg_basebackup -U "$DB_USER" -D /backup_files/"$BACKUP_DIR/backup_inc-$TIMESTAMP" -Ft -x
 
