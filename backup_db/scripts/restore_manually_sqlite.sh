98870473a658a12704b99a29306a464d064e832d
lisanaberberi <lisana.berberi@kit.edu>
2023-10-25 17:05:16 +0000

    add restore sqlite operation

---
+++ backup_db/scripts/restore_manually_sqlite.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Configuration
+DATABASE_FILE="/var/data/user-data/oidc_auth.db"
+BACKUP_DIR="/backup_files"
+RESTORE_DIR="/restore_files"
+
+# Ensure the restore directory exists
+mkdir -p "$RESTORE_DIR"
+
+# Find the most recent backup postgres file 
+RESTORE_FILE=$(ls -1t "$BACKUP_DIR"/*.tar.gz | grep -E 'sqlite-*[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.tar.gz' | head -1)
+
+if [ -n "$RESTORE_FILE" ]; then
+    # Extract the compressed backup to the restore directory
+    tar -xzf "$RESTORE_FILE" -C "$RESTORE_DIR" --strip-components=1
+
+    # Identify the extracted SQL backup file
+    SQL_BACKUP_FILE=$(ls -1 "$RESTORE_DIR" | grep '.sql$')
+    echo "sql backup file: $SQL_BACKUP_FILE"
+
+        # Restore the SQLite database
+    sqlite3 "$DATABASE_FILE'_new" < "$RESTORE_DIR/$SQL_BACKUP_FILE"
+    SQLITE_EXIT_CODE=$?
+
+    if [ $SQLITE_EXIT_CODE -eq 0 ]; then
+        echo "Restore completed successfully."
+    else
+        echo "Failed to restore the database."
+        exit 1
+    fi
+else
+    echo "No recent backup file found for restore."
+fi
