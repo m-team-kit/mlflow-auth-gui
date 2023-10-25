35e58469b021feac42a04be6fa86837bafed02b3
lisanaberberi <lisana.berberi@kit.edu>
2023-10-25 11:42:40 +0000

    modify scripts to exe backups

--- backup_db/scripts/backup_script.sh
+++ backup_db/scripts/backup_script.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 # Database details
-DB_NAME="mlflow_db"
-DB_USER="mlflow_u"
+DB_NAME="${POSTGRES_DB:?POSTGRES_DB environment variable is not set}"
+DB_USER="postgres"
 BACKUP_DIR="/backup_files"
+
+# Database connection information
+DB_HOST="database" # Name of the PostgreSQL container
+DB_PORT="5432"
+DB_PASSWORD="${POSTGRES_PASSWORD:?DATABASE_PASSWORD:?DATABASE_PASSWORD is not set}"
+
+# Construct the psql connection string
+PSQL_CONNECTION_STRING="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # Generate a timestamp for the backup file name
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 
-# Create a SQL backup using pg_dump
-pg_dump -U "$DB_USER" -d "$DB_NAME" > "$BACKUP_DIR/backup-$TIMESTAMP.sql"
+# Run pg_dump to create a backup
+pg_dump "$PSQL_CONNECTION_STRING" > "$BACKUP_DIR/backup-$TIMESTAMP.sql"
 
 # Compress the SQL backup into a .tar.gz file
 tar -czf "$BACKUP_DIR/backup-$TIMESTAMP.tar.gz" "$BACKUP_DIR/backup-$TIMESTAMP.sql"
--- backup_db/scripts/backup_script_sqlite.sh
+++ backup_db/scripts/backup_script_sqlite.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Database details
-DB_NAME="mlflow_db"
-DB_USER="mlflow_u"
+# Configuration
+DATABASE_FILE="/var/data/user-data/oidc_auth.db"
 BACKUP_DIR="/backup_files"
-
-# Generate a timestamp for the backup file name
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 
-#create the incremental backup copy
-pg_basebackup -U "$DB_USER" -D /backup_files/"$BACKUP_DIR/backup_inc-$TIMESTAMP" -Ft -x
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.sqlite"
 
+# Perform the SQLite backup 
+sqlite3 "$DATABASE_FILE" ".backup '$BACKUP_FILE'"
+
+# Compress the backup using tar 
+tar -czf "$BACKUP_DIR/backup-$TIMESTAMP.tar.gz" "backup_$TIMESTAMP.sqlite"
