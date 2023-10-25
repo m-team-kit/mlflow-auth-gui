cfeecc06af4eaff82b76070daf384c1b11f2d505
lisanaberberi <lisana.berberi@kit.edu>
2023-10-25 17:48:40 +0000

    fix issue of table already exists during restore

--- backup_db/scripts/restore_manually_sqlite.sh
+++ backup_db/scripts/restore_manually_sqlite.sh
@@ -16,18 +16,22 @@ if [ -n "$RESTORE_FILE" ]; then
     tar -xzf "$RESTORE_FILE" -C "$RESTORE_DIR" --strip-components=1
 
     # Identify the extracted SQL backup file
-    SQL_BACKUP_FILE=$(ls -1 "$RESTORE_DIR" | grep '.sql$')
+    SQL_BACKUP_FILE=$(ls -1t "$RESTORE_DIR" | grep -E 'sqlite-*[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}.sql' | head -1)
     echo "sql backup file: $SQL_BACKUP_FILE"
 
+    if [ -n "$SQL_BACKUP_FILE" ]; then
+        # Remove the existing database file
+        rm "$DATABASE_FILE"
         # Restore the SQLite database
-    sqlite3 "$DATABASE_FILE'_new" < "$RESTORE_DIR/$SQL_BACKUP_FILE"
-    SQLITE_EXIT_CODE=$?
+        sqlite3 "$DATABASE_FILE" < "$RESTORE_DIR/$SQL_BACKUP_FILE"
+        SQLITE_EXIT_CODE=$?
 
-    if [ $SQLITE_EXIT_CODE -eq 0 ]; then
-        echo "Restore completed successfully."
-    else
-        echo "Failed to restore the database."
-        exit 1
+        if [ $SQLITE_EXIT_CODE -eq 0 ]; then
+            echo "Restore completed successfully."
+        else
+            echo "Failed to restore the database."
+            exit 1
+        fi
     fi
 else
     echo "No recent backup file found for restore."
