ca0c4e74def18b7802e7751258d4bb3a72d887fc
Christophe <christophe.misc+git@protonmail.ch>
2023-11-06 14:43:33 +0100

    build: dockerfile for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  basePath: '/signup',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
